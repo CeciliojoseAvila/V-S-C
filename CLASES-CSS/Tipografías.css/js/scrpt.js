c81551ccfe4a79412daba5c0bcc18e957e503925
Cecilio José Avila Ramos <110960338+CeciliojoseAvila@users.noreply.github.com>
2022-10-08 14:11:29 -0500

    act

--- CLASES-CSS/Tipografías.css/js/scrpt.js
+++ CLASES-CSS/Tipografías.css/js/scrpt.js
@@ -4,7 +4,8 @@ function clickSobreH1() {
 mainTitle.classList.toggle("red-title");
 }
 mainTitle.addEventListener("click", clickSobreH1);*/
-
+/*
+//codigo ejemplo de formularios, hasta linea 43
 var username = document.querySelector("#username");
 var password = document.querySelector("#password");
 var loginForm = document.querySelector("#loginForm");
@@ -39,6 +40,8 @@ function submitData(event){
     //console.log("VAMOS A ENVIAR LA INFO..");
 }
 loginForm.addEventListener("submit", submitData);
+*/
+
 /*{
     var element= event.target;    
     var fieldValue = element.value;
@@ -47,3 +50,33 @@ loginForm.addEventListener("submit", submitData);
         alert("El campo es obligatorio");
     } //movemos para arriba este codigo
 })*/ //lo mismo abajo con password
+
+var user= {
+    nombre: "Cecilio",
+    apellido: "Avila",
+    edad: 35,
+    getInfo: function(){ 
+    return "Hola, soy " + this.nombre + "  " + this.apellido + " y tengo " + this.edad + " años " ;
+}
+ }
+var title= document.querySelector("#mainTitle");
+//title.innerHTML = "Bienvenido " + user.nombre + " " +user.apellido; suprimo estas dos lineas para implem el this
+//getInfo: function(){ return "Hola, soy" + this.nombre + "  " + this.apellido}
+title.innerHTML = user.getInfo();
+//ahora Arrays
+var students= [
+{fullName: "JUAN PEREZ", subject:"JavaScript"},
+{fullName: "JUANA GONZALES", subject:"HTML Y CSS"},
+{fullName: "FABIAN GOMEZ", subject:"PYTHON Y JAVA"},
+{fullName: "CECILIO AVILA", subject:"BASE DE DATOS Y VSC"},
+{fullName: "MARIA JOSE", subject:"ING. SISTEMAS Y PROGRAMACION"}
+]
+
+var studentsListUL= document.querySelector("#studentsList");
+//console.log(studentsListUL); ahora hacemos el for para iterar
+for(var i=0; i< students.length; i++){
+    //console.log(students[i])//asi me muestra todos los datos registrados
+   // console.log(students[i].fullName) //acá solo los nombres
+   studentsListUL.innerHTML += "<li>" + students[i].fullName +  "  Está estudiando " + students[i].subject + "</li>";
+
+}
